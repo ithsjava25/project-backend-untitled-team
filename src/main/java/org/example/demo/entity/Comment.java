463a69c70aec5f262a6820f805b2714e85df7b87
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-03-30 12:10:27 +0200

    Implement Comment and File JPA entities.

--- src/main/java/org/example/demo/entity/Comment.java
+++ src/main/java/org/example/demo/entity/Comment.java
@@ -1,4 +1,23 @@
 package org.example.demo.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private User author;
+
+    @ManyToOne
+    private Case caseEntity;
 }
--- src/main/java/org/example/demo/entity/File.java
+++ src/main/java/org/example/demo/entity/File.java
@@ -1,4 +1,24 @@
 package org.example.demo.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String filename;
+    private String s3key;
+
+    private LocalDateTime uploadedAt;
+
+    @ManyToOne
+    private User uploadedBy;
+
+    @ManyToOne
+    private Case associatedCase;
 }
