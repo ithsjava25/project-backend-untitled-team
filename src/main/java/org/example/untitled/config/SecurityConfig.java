a3f606dbd753e6ab39e2569814f246e86ac5b906
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-04-07 13:32:07 +0200

    Added a globalExceptionHandler.java class with error page. Added a configuration annotation to SecurityConfig and permitted images and CSS files to be read for all.

--- src/main/java/org/example/untitled/config/SecurityConfig.java
+++ src/main/java/org/example/untitled/config/SecurityConfig.java
@@ -1,13 +1,15 @@
 package org.example.untitled.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 class SecurityConfig {
 
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/home").permitAll()
+                .requestMatchers("/", "/home", "/images/**", "/css/**").permitAll()
                 .anyRequest().permitAll()
         );
 
