68ca4e9d11e1d96ed765858b77359dd3617b4238
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-04-10 12:54:20 +0200

    Added a globalExceptionHandler.java class (#33)
    * Added a globalExceptionHandler.java class with error page. Added a configuration annotation to SecurityConfig and permitted images and CSS files to be read for all.
    
    * updated code with code rabbit suggestions

---
+++ src/main/java/org/example/untitled/config/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.example.untitled.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String errorException(Exception ex){
+        log.error("error loading page, redirect to errorpage");
+        log.error(ex.getMessage());
+        return "errorpage";
+    }
+}
--- src/main/java/org/example/untitled/config/SecurityConfig.java
+++ src/main/java/org/example/untitled/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package org.example.untitled.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 class SecurityConfig {
-
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        //change later to authenticated
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/home").permitAll()
+                .requestMatchers("/", "/home", "/images/**", "/css/**").permitAll()
                 .anyRequest().permitAll()
         );
 
