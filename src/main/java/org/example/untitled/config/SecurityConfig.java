903dc3a2e5ef5a2177261a3b41e7ec04086756cd
Viktor Lindell <v.lindell@hotmail.com>
2026-04-10 17:11:20 +0200

    Resolve merge conflict in SecurityConfig, add static resource paths

--- src/main/java/org/example/untitled/config/SecurityConfig.java
+++ src/main/java/org/example/untitled/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         auth ->
-                                auth.requestMatchers("/auth/**", "/", "/home")
+                                auth.requestMatchers("/auth/**", "/", "/home", "/images/**", "/css/**")
                                         .permitAll()
                                         .anyRequest()
                                         .authenticated())
