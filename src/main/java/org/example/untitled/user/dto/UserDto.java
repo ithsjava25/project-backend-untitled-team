bb33618e7f91e1df36b9fbe7488c593738449ceb
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-04-13 14:24:49 +0200

    updated casemapper to update not assigned case to show null

--- src/main/java/org/example/untitled/user/dto/UserDto.java
+++ src/main/java/org/example/untitled/user/dto/UserDto.java
@@ -1,6 +1,5 @@
 package org.example.untitled.user.dto;
 
-import jakarta.validation.constraints.Email;
 import org.example.untitled.user.Role;
 
 import java.time.LocalDateTime;
@@ -8,7 +7,6 @@ import java.time.LocalDateTime;
 public record UserDto (
         Long id,
         String username,
-        @Email
         String email,
         Role role,
         LocalDateTime createdAt
