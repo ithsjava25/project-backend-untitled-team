d6cd96d6e6051b23310f5b4250be5df5d13784f3
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-04-20 16:46:57 +0200

    mvn spotless:apply refactoring

--- src/main/java/org/example/untitled/s3/S3Config.java
+++ src/main/java/org/example/untitled/s3/S3Config.java
@@ -1,6 +1,5 @@
 package org.example.untitled.s3;
 
-import org.example.untitled.usercase.UploadedFile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +11,6 @@ import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class S3Config {
