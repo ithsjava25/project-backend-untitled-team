05db1792e6442b0d6e52c9db3f04cb20b3774c25
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-04-21 12:50:17 +0200

    Rename `case_entity_id` to `case_id` and make `case_id` nullable in `audit_log` table.

--- src/main/resources/db/migration/V8__update_audit_log_table.sql
+++ src/main/resources/db/migration/V8__update_audit_log_table.sql
@@ -4,3 +4,6 @@ DROP CONSTRAINT IF EXISTS audit_log_user_id_fkey,
 
 ALTER TABLE audit_log
     RENAME COLUMN case_entity_id TO case_id;
+
+ALTER TABLE audit_log
+    ALTER COLUMN case_id DROP NOT NULL;
