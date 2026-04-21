fc711fbcd2184428f8e49edef75422e0eafee402
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-04-21 14:05:09 +0200

    added file to update ticket

--- src/main/java/org/example/untitled/usercase/controller/CaseViewController.java
+++ src/main/java/org/example/untitled/usercase/controller/CaseViewController.java
@@ -82,7 +82,7 @@ public class CaseViewController {
             return "edit_ticket";
         }
         try {
-            caseService.updateTicket(id, ticketForm, userDetails.getUsername());
+            caseService.updateTicket(id, ticketForm, userDetails.getUsername(), ticketForm.getFileName());
         } catch (ResponseStatusException e) {
             if (e.getStatusCode() == HttpStatus.CONFLICT) {
                 bindingResult.rejectValue("title", "error.ticketForm", e.getReason());
