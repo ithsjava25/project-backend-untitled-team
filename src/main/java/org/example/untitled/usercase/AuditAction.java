ee97db35a5a018300862096ee70bf45eee91befc
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-04-27 22:47:00 +0200

    Update AuditAction to use Locale.ROOT in getDisplayName for consistent case conversion.

--- src/main/java/org/example/untitled/usercase/AuditAction.java
+++ src/main/java/org/example/untitled/usercase/AuditAction.java
@@ -1,5 +1,7 @@
 package org.example.untitled.usercase;
 
+import java.util.Locale;
+
 public enum AuditAction {
     CASE_CREATED,
     CASE_ASSIGNED,
@@ -10,7 +12,7 @@ public enum AuditAction {
     CASE_UPDATED;
 
     public String getDisplayName() {
-        String name = this.name().toLowerCase().replace('_', ' ');
+        String name = this.name().toLowerCase(Locale.ROOT).replace('_', ' ');
         return Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
 }
