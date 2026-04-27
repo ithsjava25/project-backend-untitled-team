a85faf9aca42936aaf7109eb73b20a6cacd68074
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-04-27 10:43:28 +0200

    Add ticket view link, implement custom CommentDto query, and remove comment title from ticket template.

--- src/main/java/org/example/untitled/usercase/repository/CommentRepository.java
+++ src/main/java/org/example/untitled/usercase/repository/CommentRepository.java
@@ -3,10 +3,15 @@ package org.example.untitled.usercase.repository;
 import org.example.untitled.usercase.CaseEntity;
 import org.example.untitled.usercase.Comment;
 import org.example.untitled.usercase.dto.CommentDto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends ListCrudRepository<Comment, Long> {
+
+    @Query("SELECT new org.example.untitled.usercase.dto.CommentDto(" +
+            "c.id, c.text, c.author.id, c.author.username, c.caseEntity.id, c.createdAt) " +
+            "FROM Comment c WHERE c.caseEntity = :caseEntity")
     List<CommentDto> findCommentsByCaseEntity(CaseEntity caseEntity);
 }
