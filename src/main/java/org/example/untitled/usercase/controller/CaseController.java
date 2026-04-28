64d58b81a24a665fa2d838099a00f36610e77b1e
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-04-28 11:55:50 +0200

    updated files to show in a li format. Added favicon.ico

--- src/main/java/org/example/untitled/usercase/controller/CaseController.java
+++ src/main/java/org/example/untitled/usercase/controller/CaseController.java
@@ -28,7 +28,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
